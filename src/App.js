9efecdafc6afbbf5afb4078fb1cc519f4e2a3456
Buttman <Llagunocarl@gmail.com>
2024-08-27 12:57:29 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticlesList from "./pages/ArticlesList";
+import Article from "./pages/Article";
+import Navbar from "./components/Navbar";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticlesList />} />
+          <Route path="/article/:name" element={<Article />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Hello, Welcome to my Blog
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+        voluptas aut ex porro voluptatem, alias debitis temporibus dolorum ullam
+        neque eveniet officia numquam vel recusandae tenetur consequatur odit
+        consequuntur odio culpa doloremque repellendus cupiditate soluta? Ea
+        harum iste ex doloribus aspernatur modi, voluptatibus sit error itaque
+        minima explicabo consectetur nemo.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        consectetur alias explicabo beatae, amet veniam dolore. Optio hic
+        cupiditate, pariatur repudiandae necessitatibus odit perferendis nulla.
+        Consequatur quia, inventore illo numquam est reiciendis at porro
+        doloremque animi necessitatibus. Saepe aliquam, exercitationem sequi
+        neque officia rem quasi et culpa harum nulla voluptatum accusamus modi.
+        Quisquam natus debitis veniam voluptatibus eveniet dolores. Porro alias
+        ullam, neque in eum voluptas iste pariatur placeat expedita minus
+        debitis enim voluptate itaque, amet aliquid vitae ab hic harum accusamus
+        blanditiis optio quisquam laborum explicabo voluptatum? Iusto aliquam ab
+        repellendus! Rem quod facilis blanditiis, sequi natus officia
+        perspiciatis!
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        consectetur alias explicabo beatae, amet veniam dolore. Optio hic
+        cupiditate, pariatur repudiandae necessitatibus odit perferendis nulla.
+        Consequatur quia, inventore illo numquam est reiciendis at porro
+        doloremque animi necessitatibus. Saepe aliquam, exercitationem sequi
+        neque officia rem quasi et culpa harum nulla voluptatum accusamus modi.
+        Quisquam natus debitis veniam voluptatibus eveniet dolores. Porro alias
+        ullam, neque in eum voluptas iste pariatur placeat expedita minus
+        debitis enim voluptate itaque, amet aliquid vitae ab hic harum accusamus
+        blanditiis optio quisquam laborum explicabo voluptatum? Iusto aliquam ab
+        repellendus! Rem quod facilis blanditiis, sequi natus officia
+        perspiciatis!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
